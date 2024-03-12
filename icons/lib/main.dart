1dfea7f2ded758a044c1b37f03509881eb4dd775
syimyk <programmist00707@gmail.com>
2024-03-12 16:25:20 +0600

    done

--- icons/lib/main.dart
+++ icons/lib/main.dart
@@ -1,21 +1,21 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return:MaterialApp(
-//       home: Scaffold(
-//       appBar: AppBar(
+  @override
+  Widget build(BuildContext context) {
+    return:MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
 
-//       ),
-//       ),
-//     );
-//   }
-// }
+      ),
+      ),
+    );
+  }
+}

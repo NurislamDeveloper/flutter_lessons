06f19a916ab664aa369f2f1a7c230086a35b7e87
syimyk <programmist00707@gmail.com>
2024-03-13 13:37:40 +0600

    do

---
+++ my_project/lib/account_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/first_project.dart';
+
+class AccountPaage extends StatefulWidget {
+  const AccountPaage({super.key});
+
+  @override
+  State<AccountPaage> createState() => _AccountPaageState();
+}
+
+class _AccountPaageState extends State<AccountPaage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        //here I changed size of appbar
+        preferredSize: const Size(double.infinity, 60),
+        child: AppBar(
+          automaticallyImplyLeading: false, //here I delated arrow defoult
+          backgroundColor: Colors.blue,
+          actions: [IconButton(onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()),
+                (route) => false, //in here I move from account to first page I use Navigator pop in here but ut does't work like how I want in here
+              );
+          }, icon:const Padding(
+            padding: EdgeInsets.fromLTRB(0,0,350,200),
+            child: Icon(Icons.arrow_back_outlined,color: Colors.white,size: 28,),
+          )
+            ),
+           ],
+          ),
+        ),
+    );
+  }
+}
---
+++ my_project/lib/first_project.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/account_page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      drawer: Drawer(
+        backgroundColor: const Color.fromARGB(255, 43, 86, 213),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AccountPaage()));
+                },
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 220,
+                      100), // here I moved icon from centere to the left side.
+                  child: Icon(
+                    Icons.account_circle_outlined,
+                    color: Colors.white,
+                    size: 56,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text(
+                "home",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+             
+            ),
+            ListTile(
+              title: const Text(
+                "business",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text(
+                "school",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          //here I change color of drawer from black to the white,
+          color: Colors.white,
+          size: 26,
+        ),
+        backgroundColor: const Color.fromARGB(
+          255,
+          5,
+          113,
+          255,
+        ),
+        centerTitle: true,
+        title: const Text(
+          "My Project",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 26,
+            fontWeight: FontWeight.w200,
+          ),
+        ),
+      ),
+    );
+  }
+}

c8a7c3fbb59868f6831417f6a12f2e723fb9127f
Nurislam <nurislam.temirbekov.bj@gmail.com>
2024-04-11 19:30:02 +0600

    done

---
+++ funtions_practice/lib/my_app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(
+          255,
+          8,
+          43,
+          72,
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
---
+++ funtions_practice/lib/my_home_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  void increment(int count) {
+    setState(() {});
+  }
+
+  void decrement(count) {
+    setState(() {});
+  }
+
+  void result() {
+    if (count == 10 || count == -10) {
+       showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content:  Text("$count"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "F U N T I O N S",
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Numbers: $count",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+            const Gap(
+              10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                  ),
+                  onPressed: () {
+                    decrement(count--);
+                    result();
+                  },
+                  child: const Icon(
+                    Icons.remove,
+                    color: Colors.white,
+                  ),
+                ),
+                const Gap(
+                  20,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                  ),
+                  onPressed: () {
+                    increment(count++);
+                    result();
+                  },
+                  child: const Icon(
+                    Icons.add,
+                    size: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
